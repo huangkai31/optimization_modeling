c90723c77ead77a9161babe94d9b181b4de7da46
Kai Huang <huangkai39@126.com>
2023-11-10 16:34:51 +0800

    利润最大化

--- pages/01_线性规划_利润最大化.py
+++ pages/01_线性规划_利润最大化.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+from pyomo.environ import *
 
 
 st.set_page_config(
@@ -7,7 +7,16 @@ st.set_page_config(
     layout="wide"
 )
 
-from pyomo.environ import *
+st.write("""
+         假设你正在考虑创办一家生产X产品的企业。你已经确定X的市场每周最多40台，每台270美元。
+         每台机组的生产需要100美元的原材料、1小时的A类劳动力和2小时的B类劳动力。您可以无限量地使用原材料，
+         但每周只有80小时的劳动力A，成本为50美元/小时，每周只有100小时的劳动力B，成本为40美元/小时。
+         忽略所有其他费用，每周最大利润是多少？
+         """)
+
+raw_cost = st.number_input('原材料成本', 100)
+a_cost = st.number_input('劳动力A 成本', 40)
+
 model = ConcreteModel()
 
 # declare decision variables
